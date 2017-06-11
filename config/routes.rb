f0e6b577d7e3af560abd1cf0049138d148384da1
John Perez <jwperez1@yahoo.com>
2017-06-11 22:22:23 +0000

    Make races default page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :runners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'runners#index'
+  root 'races#index'
 end
