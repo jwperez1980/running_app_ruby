d20dddffcba0e21daa141cbdc8db68bde7b70b4b
John Perez <jwperez1@yahoo.com>
2017-06-27 21:02:08 +0000

    Add rack-cors gem and update application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,17 +6,23 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Testit6
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     
     config.action_dispatch.default_headers = {
     'Access-Control-Allow-Origin' => 'http://localhost:3000',
-    'Access-Control-Allow-Origin' => 'http://localhost:63288',
-    'Access-Control-Allow-Origin' => 'https://quiet-refuge-72491.herokuapp.com',
-    'Access-Control-Allow-Origin' => 'https://data-engine.herokuapp.com',
     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
   }
   end
